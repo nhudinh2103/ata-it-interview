86ef97a4e921350e773717f37896b4028bd67a38
dinhnn <nhudinh2103@gmail.com>
2024-01-31 18:58:14 +0700

    Temporary set endpoint unauthenticated (for dev and test)

--- backend/src/main/java/com/ata/interview/config/SecurityConfig.java
+++ backend/src/main/java/com/ata/interview/config/SecurityConfig.java
@@ -2,7 +2,10 @@ package com.ata.interview.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
@@ -11,6 +14,15 @@ public class SecurityConfig {
 	@Bean
 	public WebSecurityCustomizer webSecurityCustomizer() {
 		return (web) -> web.ignoring().requestMatchers(new AntPathRequestMatcher("/h2-console/**"));
+	}
+	
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		
+		http.csrf((csrf) -> csrf.disable()).cors(Customizer.withDefaults())
+				.authorizeHttpRequests((authorizeHttpRequests) -> authorizeHttpRequests.anyRequest().permitAll());
+		
+		return http.build();
 	}
 
 }
