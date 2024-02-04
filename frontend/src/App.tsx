fc6ab6588f27ea499ed4edd12d2196eb8e73db23
dinhnn <nhudinh2103@gmail.com>
2024-02-04 14:33:23 +0700

    frontend > Add search orders by start date and end date.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,11 @@
-import { useState } from 'react'
+import { useState } from "react";
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 // import './App.css'
-import CustomSearch from './CustomSearch'
+import CustomSearch from "./CustomSearch";
 
+import { LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 
 import {
   AppBar,
@@ -15,16 +17,20 @@ import {
 
 function App() {
   return (
-    <Container maxWidth="xl">
-        <CssBaseline />
-        <AppBar position="static">
+    <>
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <Container maxWidth="xl">
+          <CssBaseline />
+          <AppBar position="static">
             <Toolbar>
-                <Typography variant="h6">ATA-IT bank</Typography>
+              <Typography variant="h6">ATA-IT bank</Typography>
             </Toolbar>
-        </AppBar>
-            <CustomSearch />
-    </Container>
-);
+          </AppBar>
+          <CustomSearch />
+        </Container>
+      </LocalizationProvider>
+    </>
+  );
 }
 
-export default App
+export default App;
--- frontend/src/CustomSearch.tsx
+++ frontend/src/CustomSearch.tsx
@@ -2,55 +2,159 @@ import { useEffect, useState } from "react";
 
 import Table from "react-bootstrap/Table";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { Button } from "react-bootstrap";
+import { Input, MenuItem, Select } from "@mui/material";
+import { DatePicker } from "@mui/x-date-pickers";
+import moment from "moment";
 
 interface UserData {
-  account: string,
-  operation: string,
-  symbol: string,
-  description: string,
-  qty: string,
-  filledQty: string,
-  price: string,
-  status: string,
-  date: string,
-  expiration: string,
-  noRef: string,
-  extRef: string
+  account: string;
+  operation: string;
+  symbol: string;
+  description: string;
+  qty: string;
+  filledQty: string;
+  price: string;
+  status: string;
+  date: string;
+  expiration: string;
+  noRef: string;
+  extRef: string;
 }
 
+const dummyData = [
+  {
+    account: "00000001",
+    operation: "Buy",
+    symbol: "NA",
+    description: "NATIONAL BANK OF CDA",
+    qty: "5",
+    filledQty: "0",
+    price: "526.00",
+    status: "Waiting",
+    date: "2022/12/08 05:12:36",
+    expiration: "2022/12/08 05:12:36",
+    noRef: "13830581",
+    extRef: "2-XXXXXXX1-1",
+  },
+  {
+    account: "00000002",
+    operation: "Buy",
+    symbol: "NA",
+    description: "NATIONAL BANK OF CDA",
+    qty: "90",
+    filledQty: "0",
+    price: "744.00",
+    status: "Waiting",
+    date: "2022/12/15 23:30:32",
+    expiration: "2022/12/15 23:30:32",
+    noRef: "20435409",
+    extRef: "2-XXXXXXX1-2",
+  },
+  {
+    account: "00000003",
+    operation: "Buy",
+    symbol: "NA",
+    description: "NATIONAL BANK OF CDA",
+    qty: "60",
+    filledQty: "0",
+    price: "369.00",
+    status: "Waiting",
+    date: "2023/01/04 02:57:35",
+    expiration: "2023/01/04 02:257:35",
+    noRef: "09612775",
+    extRef: "2-XXXXXXX1-3",
+  },
+];
+
 function CustomSearch() {
-  const [userData, setUserData] = useState<UserData[]>([]);
+  const [userData, setUserData] = useState<UserData[]>(dummyData);
+  const [userSearchData, setUserSearchData] = useState<UserData[]>(dummyData);
+
   const [name, setName] = useState<string>("");
 
-  useEffect(() => {
-    const data = [
-      { account: "00000001", operation: "Buy", symbol: "NA", description: "NATIONAL BANK OF CDA", qty: "5", filledQty: "0", price: "526.00", status: "Waiting", date: "2022/12/22/05:12:36", expiration: "2022/12/22/05:12:36", noRef: "13830581", extRef: "2-XXXXXXX1-1"},
-      { account: "00000001", operation: "Buy", symbol: "NA", description: "NATIONAL BANK OF CDA", qty: "5", filledQty: "0", price: "526.00", status: "Waiting", date: "2022/12/22/05:12:36", expiration: "2022/12/22/05:12:36", noRef: "13830581", extRef: "2-XXXXXXX1-1"},
-    ];
-
-    setUserData(data);
-  }, []);
+  const [fromDate, setFromDate] = useState<string | null>("");
+  const [toDate, setToDate] = useState<string | null>("");
 
   const handleSearch = () => {
-    alert("clicked");
+
+    let fromDateNumVal = -1;
+    if (fromDate !== "" && fromDate !== null) {
+      fromDateNumVal = new Date(fromDate).getTime();
+    }
+
+    let toDateNumVal = Number.MAX_SAFE_INTEGER;
+    if (toDate !== "" && toDate !== null) {
+      toDateNumVal = new Date(toDate).getTime();
+    }
+
+    if (fromDateNumVal > toDateNumVal) {
+      alert("invalid search: from date is after to date => Please recheck against");
+      return;
+    }
+
+    const newData = userData
+      .filter((x) => x.account == (name == "" ? x.account : name))
+      .filter((x) => {
+        let rowDateNumVal = moment(x.date, "YYYY/MM/DD HH:mm:ss").valueOf();
+
+        return fromDateNumVal <= rowDateNumVal && rowDateNumVal <= toDateNumVal;
+      });
+    setUserSearchData(newData);
+
   };
 
   return (
     <>
       <Table>
         <tbody>
-        <tr>
-          <td>
-            <input
-              type="text"
-              placeholder="Enter name..."
-              onChange={(e) => setName(e.target.value)}
-            ></input>
-          </td>
-          <td>
-            <button onClick={() => handleSearch}> Search</button>
-          </td>
-        </tr>
+          <tr>
+            <td>
+              <Input
+                className="form-control"
+                type="text"
+                placeholder="Enter account no..."
+                onChange={(e) => setName(e.target.value)}
+              ></Input>
+            </td>
+            <td>
+              <Select defaultValue="transmission">
+                <option value="transmission">Transmission</option>
+                {/* <MenuItem value='transmission'>Transmission</MenuItem> */}
+              </Select>
+
+              <Select defaultValue="waiting">
+                <option value="waiting">Waiting</option>
+                {/* <MenuItem value='transmission'>Transmission</MenuItem> */}
+              </Select>
+
+            </td>
+            <td>
+              <DatePicker
+                label="Start Date"
+                value={fromDate}
+                onChange={(newValue) => {
+                  setFromDate(newValue);
+                }}
+                format="DD/MM/YYYY"
+              />
+            </td>
+            <td>
+              <DatePicker
+                label="End Date"
+                value={toDate}
+                onChange={(newValue) => {
+                  setToDate(newValue);
+                }}
+                format="DD/MM/YYYY"
+              />
+            </td>
+            <td>
+              <Button className="btn btn-primary" onClick={handleSearch}>
+                Search
+              </Button>
+            </td>
+          </tr>
         </tbody>
       </Table>
 
@@ -72,8 +176,8 @@ function CustomSearch() {
           </tr>
         </thead>
         <tbody>
-          {userData && userData.length > 0
-            ? userData.map((item) => (
+          {userSearchData && userSearchData.length > 0
+            ? userSearchData.map((item) => (
                 <tr key={item.account}>
                   <td>{item.account}</td>
                   <td>{item.operation}</td>
